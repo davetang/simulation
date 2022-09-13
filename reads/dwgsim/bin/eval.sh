69352f4077bf46c975a7a152b43fe0853c8c1040
Dave Tang <davetingpongtang@gmail.com>
2022-09-14 08:56:26 +0900

    Compare indels separately

--- reads/dwgsim/bin/eval.sh
+++ reads/dwgsim/bin/eval.sh
@@ -20,6 +20,9 @@ base=$(basename ${bam} .bam)
 ${bin}/dwgsim_eval ${bam} |\
    gzip > ${root}/test/${base}.eval.txt.gz
 
+${bin}/dwgsim_eval -i ${bam} |\
+   gzip > ${root}/test/${base}.indels.eval.txt.gz
+
 >&2 echo Done
 exit 0
 
