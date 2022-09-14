f9f066f7561f85d860b862bc407b0b3fdd059713
Dave Tang <davetingpongtang@gmail.com>
2022-09-14 14:07:50 +0900

    Summarise bam reads

---
+++ reads/dwgsim/bin/bam_summary.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+from collections import defaultdict
+# https://pysam.readthedocs.io/en/latest/api.html
+import pysam
+
+parser = argparse.ArgumentParser(description =
+    """
+    Read count summary of mapped dwgsim generated reads grouped by
+    read pair, variant type, and number of sequencing errors""",
+    formatter_class = argparse.RawTextHelpFormatter
+)
+parser.add_argument(
+    "bam",
+    help = "mapping results in BAM format of dwgsim generated reads",
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    help = "verbose mode (default = False)",
+    default = False,
+    action = "store_true"
+)
+parser.add_argument(
+        "-p",
+        "--threads",
+        help = "number of threads (default = 1)",
+        choices = range(1,9),
+        default = 1,
+        type = int
+)
+args = parser.parse_args()
+
+if args.verbose:
+    print("Verbose mode", file = sys.stderr)
+    print("Using {} threads".format(args.threads), file = sys.stderr)
+    print("Input: {}".format(args.bam), file = sys.stderr)
+
+bam = pysam.AlignmentFile(args.bam, "rb")
+
+stats = [{}, {}, {}]
+
+for read in bam.fetch():
+
+    if read.flag & 0x40:
+        read_pair = 0
+    elif read.flag & 0x80:
+        read_pair = 1
+    else:
+        print("Could not determine read pair", file = sys.stderr)
+        sys.exit()
+
+    my_split = read.query_name.replace(":", "_").split("_")
+
+    # contigs can have underscores, which causes a variable number of splits
+    assert len(my_split) >= 14, my_split
+    # assign backwards
+    my_split.reverse()
+    my_split[1:13] = [ int(x) for x in my_split[1:13] ]
+    read_id, indel2, snp2, seq_error2, indel1, snp1, seq_error1, random2, \
+    random1, strand2, strand1, start2, start1 = my_split[0:13]
+    contig = '_'.join(my_split[13:])
+
+    if read_pair == 0 and indel1 > 0 and snp1 > 0:
+        var = 'both'
+    elif read_pair == 1 and indel2 > 0 and snp2 > 0:
+        var = 'both'
+    elif indel1 > 0 or indel2 > 0:
+        var = 'indel'
+    elif snp1 > 0 or snp2 > 0:
+        var = 'snp'
+    else:
+        var = 'none'
+
+    if read_pair == 0:
+        seq_error = seq_error1
+    else:
+        seq_error = seq_error2
+
+    if var in stats[read_pair]:
+        if seq_error in stats[read_pair][var]:
+            stats[read_pair][var][seq_error] += 1
+        else:
+            stats[read_pair][var][seq_error] = 1
+    else:
+        stats[read_pair][var] = defaultdict(dict)
+        stats[read_pair][var][seq_error] = 1
+
+for idx, read_pair in enumerate(stats):
+    for var, seq_error in sorted(read_pair.items()):
+        for seq_error, value in sorted(seq_error.items()):
+            print(f"read{idx+1}\t{var}\t{seq_error}\t{value}")
+
+bam.close()
+sys.exit()
+
