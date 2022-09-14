431acd8022ab7ac6b2b8cd9c7d8a370450e7140c
Dave Tang <davetingpongtang@gmail.com>
2022-09-14 14:07:26 +0900

    Save incorrectly mapped reads

--- reads/dwgsim/bin/eval.sh
+++ reads/dwgsim/bin/eval.sh
@@ -11,6 +11,7 @@ if [[ $# < 1 ]]; then
    usage
 fi
 
+threads=8
 bin=$(dirname $0)
 root=${bin}/..
 
@@ -22,6 +23,16 @@ ${bin}/dwgsim_eval ${bam} |\
 
 ${bin}/dwgsim_eval -i ${bam} |\
    gzip > ${root}/test/${base}.indels.eval.txt.gz
+
+${bin}/dwgsim_eval -p ${bam} |\
+   ${bin}/samtools sort -@ ${threads} -O BAM |\
+   tee ${root}/test/${base}.incorrect.bam |\
+   ${bin}/samtools index - ${root}/test/${base}.incorrect.bam.bai
+
+${bin}/dwgsim_eval -p -i ${bam} |\
+   ${bin}/samtools sort -@ ${threads} -O BAM |\
+   tee ${root}/test/${base}.incorrect.indels.bam |\
+   ${bin}/samtools index - ${root}/test/${base}.incorrect.indels.bam.bai
 
 >&2 echo Done
 exit 0
