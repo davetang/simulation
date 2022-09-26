ed7f785b6da17aa037c8ee1f0adbece6d4810283
Dave Tang <davetingpongtang@gmail.com>
2022-09-26 14:26:26 +0900

    More options

--- reads/dwgsim/bin/sim.sh
+++ reads/dwgsim/bin/sim.sh
@@ -18,7 +18,15 @@ fi
 # -2 INT length of the second read [70]
 # -e FLOAT per base/color/flow error rate of the first read [from 0.020 to 0.020 by 0.000]
 # -E FLOAT per base/color/flow error rate of the second read [from 0.020 to 0.020 by 0.000]
+
+# note that the mutation rate is with respect to the reference FASTA. The
+# default rate of 0.0010 will generate around 3 million mutations in a human
+# reference genome
+# -r FLOAT rate of mutations [0.0010]
+
 # -R FLOAT fraction of mutations that are indels [0.10]
+# -X FLOAT probability an indel is extended [0.30]
+# -I INT the minimum length indel [1]
 # -y FLOAT probability of a random DNA read [0.05]
 # -z INT random seed (-1 uses the current time) [-1]
 
